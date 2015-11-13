f2c6a85f5626ff3b53b06fbbb9fd1111efa52ab7
Roaim Ahmed Hridoy <rah0024hr@gmail.com>
2015-11-14 05:27:40 +0600

    Initial commit

---
+++ app/src/main/java/tk/roaimsapp/noxml/MainActivity.java
@@ -0,0 +1,14 @@
+package tk.roaimsapp.noxml;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
